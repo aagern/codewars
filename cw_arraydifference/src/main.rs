d907fab370dbed8cc7c9cb6a58f222fe105cc17d
Alex Rybalko <aagern@gmail.com>
2025-12-04 20:55:16 +0300

    solution midified with time measure

--- cw_arraydifference/src/main.rs
+++ cw_arraydifference/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeSet;
 use std::time::Instant;
 
 fn array_diff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
@@ -9,6 +10,16 @@ fn array_diff2<T: PartialEq>(mut a: Vec<T>, b: Vec<T>) -> Vec<T> {
     a
 }
 
+fn array_diff3<T>(a: Vec<T>, b: Vec<T>) -> Vec<T>
+where
+    T: PartialEq + Ord + Clone,
+{
+    let removal_list: BTreeSet<_> = b.iter().cloned().collect();
+    a.into_iter()
+        .filter(|x| !removal_list.contains(x))
+        .collect()
+}
+
 fn main() {
     let start = Instant::now();
     println!("{:?}", array_diff(vec![1, 2, 2], vec![1]));
@@ -18,9 +29,17 @@ fn main() {
     println!("{:?}", array_diff2(vec![1, 2, 2], vec![1]));
     let duration_b = start.elapsed();
 
+    let start = Instant::now();
+    println!("{:?}", array_diff3(vec![1, 2, 2], vec![1]));
+    let duration_c = start.elapsed();
+
     println!("Function diff1: {:?}", duration_a);
     println!("Function diff2: {:?}", duration_b);
+    println!("Function diff3: {:?}", duration_c);
 
-    //Function array_diff1: 653.89µs
-    //Function array_diff2: 3.93µs => x200 faster!
+    /* cargo run --release
+    Function diff1: 342.077µs
+    Function diff2: 2.489µs
+    Function diff3: 2.37µs
+    */
 }
