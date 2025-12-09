f8519ac04796a6f52190c10085286be2a89dfcc3
Alex Rybalko <aagern@gmail.com>
2025-12-09 11:38:47 +0300

    bit count solution added

---
+++ cw_bitcount/src/main.rs
@@ -0,0 +1,12 @@
+#[allow(unused)]
+fn count_bits(n: i64) -> u32 {
+    format!("{:b}", n).matches('1').count() as u32
+}
+
+fn count_bits2(n: i64) -> u32 {
+    n.count_ones()
+}
+
+fn main() {
+    println!("{}", count_bits2(1234));
+}
