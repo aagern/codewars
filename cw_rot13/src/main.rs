2ce52ba318ecf97038a030985d4a11f27ef5cad6
Alex Rybalko <aagern@gmail.com>
2025-12-10 20:42:14 +0300

    Rot13 Caesar cipher solved

---
+++ cw_rot13/src/main.rs
@@ -0,0 +1,14 @@
+fn rot13(message: &str) -> String {
+    message
+        .chars()
+        .map(|c| match c {
+            'a'..='m' | 'A'..='M' => (c as u8 + 13) as char,
+            'n'..='z' | 'N'..='Z' => (c as u8 - 13) as char,
+            _ => c,
+        })
+        .collect()
+}
+
+fn main() {
+    println!("{}", rot13("Test")); // Grfg
+}
