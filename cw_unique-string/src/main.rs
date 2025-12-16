31dd2a0c59034e072cf25721fab0f090be9db3f2
Alex Rybalko <aagern@gmail.com>
2025-12-16 18:24:09 +0300

    Unique string task solved

--- cw_unique-string/src/main.rs
+++ cw_unique-string/src/main.rs
@@ -1,9 +1,18 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn sort_chars(s: &str) -> String {
-    let mut char_sort = s.chars().collect::<Vec<char>>();
-    char_sort.sort_unstable();
-    char_sort.into_iter().collect::<String>()
+    //let char_sort = s.to_lowercase().chars().collect::<HashSet<char>>();
+    //let mut char_sort = char_sort.into_iter().collect::<Vec<char>>();
+
+    let mut char_sort = s
+        .chars()
+        .filter(|&c| c != ' ')
+        .map(|c| c.to_ascii_lowercase())
+        .collect::<HashSet<char>>()
+        .into_iter()
+        .collect::<Vec<char>>();
+    char_sort.sort_unstable(); // sort in place, does not return vector
+    char_sort.into_iter().collect::<String>() // return to String
 }
 
 pub fn find_uniq<'a>(arr: &'a [&str]) -> &'a str {
